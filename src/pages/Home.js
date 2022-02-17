9d42e381a44435b6a4f482d33f7f22ad3c590ca8
Pancho Labrador <plabradorjr@gmail.com>
2022-02-17 00:11:40 -0600

    linktree done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,84 @@
 import React, { Component } from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import './styles.css'
+import pic from '../assets/cupcat.png'
 
 
 export default class Home extends Component {
 
     render() {
         return (
-            <Container fluid>
-                <Row>
-                    <Col className='text-center'>1 of 2</Col>
-                    <Col className='text-center'>2 of 2</Col>
-                </Row>
-                <Row>
-                    <Col className='text-center'>1 of 3</Col>
-                    <Col className='text-center'>2 of 3</Col>
-                    <Col className='text-center'>3 of 3</Col>
-                </Row>
-          </Container>
+            <div className='container-fluid'>
+                <div className='row text-center'>
+                    <div className='col'>
+                        <img src={pic} alt="profile pic" className='rounded-circle' id='cat'/>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                    </div>
+                    <div className='col'>
+                        <div className='row'>
+                            <button type="button" id="btn-1">
+                                Portfolios
+                            </button>
+                        </div>
+                    </div>
+                    <div className='col'>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                    </div>
+                    <div className='col'>
+                        <div className='row'>
+                            <button type="button" id="btn-2">
+                                Github
+                            </button>
+                        </div>
+                    </div>
+                    <div className='col'>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                    </div>
+                    <div className='col'>
+                        <div className='row'>
+                            <button type="button" id="btn-2">
+                                LinkedIn
+                            </button>
+                        </div>
+                    </div>
+                    <div className='col'>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                    </div>
+                    <div className='col'>
+                        <div className='row'>
+                            <button type="button" id="btn-2">
+                                Twitter
+                            </button>
+                        </div>
+                    </div>
+                    <div className='col'>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col'>
+                    </div>
+                    <div className='col'>
+                        <div className='row'>
+                            <button type="button" id="btn-2">
+                                Resume
+                            </button>
+                        </div>
+                    </div>
+                    <div className='col'>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
