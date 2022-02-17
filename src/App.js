47574adcd95849d26fe11d6ce49a76492deeef90
Pancho Labrador <plabradorjr@gmail.com>
2022-02-16 21:31:16 -0600

    added react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './quick-website-dark.css';
 import Home from './pages/Home'
 
 function App() {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
 
 export default class Home extends Component {
 
-
     render() {
         return (
-            <div className="card">
-            <div className="card-header py-4" id="heading-2-1" data-toggle="collapse" role="button" data-target="#collapse-2-1" aria-expanded="false" aria-controls="collapse-2-1">
-                <h6 className="mb-0"><i data-feather="file" className="mr-3"></i>Which license do I need?</h6>
-            </div>
-            <div id="collapse-2-1" className="collapse" aria-labelledby="heading-2-1" data-parent="#accordion-2">
-                <div className="card-body">
-                    <p>Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et.</p>
-                </div>
-            </div>
-        </div>
+            <Container fluid>
+                <Row>
+                    <Col className='text-center'>1 of 2</Col>
+                    <Col className='text-center'>2 of 2</Col>
+                </Row>
+                <Row>
+                    <Col className='text-center'>1 of 3</Col>
+                    <Col className='text-center'>2 of 3</Col>
+                    <Col className='text-center'>3 of 3</Col>
+                </Row>
+          </Container>
         )
     }
 }
