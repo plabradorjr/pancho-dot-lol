c1c4a67e22795723e602914b734f0e5c1e1de8f2
Pancho Labrador <plabradorjr@gmail.com>
2022-02-17 23:51:44 -0600

    portfolio card done

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,43 @@
 import React from 'react'
+import './card.css'
+const Card = ({title, description, stack, website, github, youtube, image}) => {
 
-const Card = ({title, description, stack, website, github}) => {
-  
+    let youtubeButton;
+    if (youtube) {
+        youtubeButton = <div className="col-sm-4"><a href={youtube} target="_blank" rel="noreferrer" className="btn btn-block btn-sm btn-outline-white">YouTube</a></div>;
+    }
+
+    let imageBanner;
+    if (image) {
+      imageBanner = (<div className="position-relative overflow-hidden">
+                        <img alt="placeholder" src={image} className="card-img-top"/>
+                    </div>)
+    }
+
+    let githubButton;
+    if (github) {
+      githubButton = (<div className="col-sm-4">
+                        <a href={github} target="_blank" rel="noreferrer" className="btn btn-block btn-sm btn-outline-white mb-1">GitHub</a>
+                      </div>)
+    }
+
     return (
-      <div className='col-md-6'>
-            <div className='card'>
+      <div className='col-md-8'>
+            <div className='card' id='card-blurr'>
+                        {imageBanner}
                 <div className='card-body'>
-                        <a href="" className='d-block h5 mt-3'>{title}</a>
+                        <div className='d-block h5 mt-3'>{title}</div>
                         <p className="text-sm text-muted">
                             {description}
                         </p>
-                    <div className="row align-items-center mt-4">
-                            <div className="col-6">
-                                <a href={website} className="btn btn-sm btn-primary">Live website</a>
+                        <h6>Stack:</h6>
+                        <p className="text-sm text-muted">{stack}</p>
+                    <div className="row mt-4">
+                            <div className="col-sm-4">
+                                <a href={website} target="_blank" rel="noreferrer" className="btn btn-block btn-sm mb-1" id='btn-gradient'>Live website</a>
                             </div>
-                            <div className="col-6">
-                                <a href={github} className="btn btn-sm btn-secondary">GitHub</a>
-                            </div>
+                            {githubButton}
+                            {youtubeButton}
                     </div>
                 </div>
             </div>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,25 +1,69 @@
 import React, { Component } from 'react'
 import Card from '../components/Card'
+import ethereum from '../assets/ethereum.jpg'
+import portfolio2 from '../assets/portfolio2.png'
+import surgiwiki from '../assets/surgiwiki.png'
+import portfolio4 from '../assets/portfolio4.jpg'
+
 
 export default class Portfolio extends Component {
   render() {
     return (
       <div className='container'>
-          <div className='row'>
+          <div className='row justify-content-center'>
             <Card
-                title="ldld..."
-                description={"dfasdfasf"}
-                website={"https://youtube.com"}
-                github={"https://github.com"}
+                title="Decentralized Exchange"
+                description="Live on Kovan Ethereum testnet. Deployed ERC20 token with symbol $LMAO. 
+                            Deployed exchange contract. Users can trade $LMAO token againts Testnet ETH. 
+                            Users can place buy/sell orders, or market buy/sell existing orders.
+                            A candle chart is available to show recent trade history."
+                stack={"Solidity, React, Redux, Ganache, Infura, Truffle, Bootstrap, Netlify"}
+                website="https://lmao.shill.lol"
+                github={"https://github.com/plabradorjr/lmao-token-and-exchange"}
+                image={ethereum}
             />
-             <Card
-                title="wtf..."
-                description={"dfasdfasf"}
-                website={"https://youtube.com"}
-                github={"https://github.com"}
-            />
-          
             
+          </div>
+          <div className='row justify-content-center'>
+            <Card
+                title="Trending Cryptocurrency Currator"
+                description="Users can view trending coins from Coingecko API,
+                            search thousands of other cryptocurrencies,
+                            post comments, login via twitter, and interact with
+                            tradingview candle charts."
+                website={"https://app.shill.lol"}
+                github={"https://github.com/plabradorjr/shill_v0.2"}
+                stack="React, Redux, Ruby on Rails, HighCharts, Bootstrap,
+                            API, OAuth gem, Heroku, Netlify"
+                image={portfolio2}
+                youtube="https://youtu.be/B1-YHROsui8"
+            />
+          </div>
+          <div className='row justify-content-center'>
+            <Card
+                title="Content and Photo Management System for Nurses"
+                description="Users can create notes and inventories. Allows 
+                            image upload hosted by Amazon Web Services.
+                            Secured third party login via Twitter."
+                stack={"Ruby on Rails, Amazon Web Services, SQL, Bootstrap"}
+                website="https://surgi.wiki"
+                github={"https://github.com/plabradorjr/surgi_app_rails"}
+                image={surgiwiki}
+                youtube="https://youtu.be/MkMiXls_E7g"
+            />
+          </div>
+          <div className='row justify-content-center'>
+            <Card
+                title="E-commerce Store"
+                description="Fully functional and automated e-commerce store.
+                            Users around the world can purchase t-shirts and
+                            all the fullfillments are automated. Approximate 
+                            delivery in 10-20 business days. Credit cards and 
+                            major cryptocurrencies are accepted as form of payments."
+                stack={"Wordpress, Stripe, Coinbase, WooCommerce"}
+                website="https://shill.lol"
+                image={portfolio4}
+            />
           </div>
            
       </div>
