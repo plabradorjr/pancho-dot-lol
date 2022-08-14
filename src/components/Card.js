bed703a31dcf18e9eac82cc2281d5e9e4a978503
Pancho Labrador <plabradorjr@gmail.com>
2022-08-14 05:28:29 -0500

    portfolio removed images

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,49 +1,83 @@
-import React from 'react'
-import './card.css'
-const Card = ({title, description, stack, website, github, youtube, image}) => {
+import React from "react";
+import "./card.css";
+const Card = ({
+  title,
+  description,
+  stack,
+  website,
+  github,
+  youtube,
+  image,
+}) => {
+  let youtubeButton;
+  if (youtube) {
+    youtubeButton = (
+      <div className="col-sm-4">
+        <a
+          href={youtube}
+          target="_blank"
+          rel="noreferrer"
+          className="btn btn-block btn-sm btn-outline-white"
+        >
+          YouTube
+        </a>
+      </div>
+    );
+  }
 
-    let youtubeButton;
-    if (youtube) {
-        youtubeButton = <div className="col-sm-4"><a href={youtube} target="_blank" rel="noreferrer" className="btn btn-block btn-sm btn-outline-white">YouTube</a></div>;
-    }
+  let imageBanner;
+  if (image) {
+    imageBanner = (
+      <div className="position-relative overflow-hidden">
+        <img alt="placeholder" src={image} className="card-img-top" />
+      </div>
+    );
+  }
 
-    let imageBanner;
-    if (image) {
-      imageBanner = (<div className="position-relative overflow-hidden">
-                        <img alt="placeholder" src={image} className="card-img-top"/>
-                    </div>)
-    }
+  let githubButton;
+  if (github) {
+    githubButton = (
+      <div className="col-sm-4">
+        <a
+          href={github}
+          target="_blank"
+          rel="noreferrer"
+          className="btn btn-block btn-sm btn-outline-white mb-1"
+        >
+          GitHub
+        </a>
+      </div>
+    );
+  }
 
-    let githubButton;
-    if (github) {
-      githubButton = (<div className="col-sm-4">
-                        <a href={github} target="_blank" rel="noreferrer" className="btn btn-block btn-sm btn-outline-white mb-1">GitHub</a>
-                      </div>)
-    }
-
-    return (
-      <div className='col-md-8'>
-            <div className='card' id='card-blurr'>
-                        {imageBanner}
-                <div className='card-body'>
-                        <div className='d-block h5 mt-3'>{title}</div>
-                        <p className="text-sm text-muted">
-                            {description}
-                        </p>
-                        <h6>Stack:</h6>
-                        <p className="text-sm text-muted">{stack}</p>
-                    <div className="row mt-4">
-                            <div className="col-sm-4">
-                                <a href={website} target="_blank" rel="noreferrer" className="btn btn-block btn-sm mb-1" id='btn-gradient'>Live website</a>
-                            </div>
-                            {githubButton}
-                            {youtubeButton}
-                    </div>
-                </div>
+  return (
+    <div className="col-md-6">
+      <div className="card" id="card-blurr">
+        {imageBanner}
+        <div className="card-body">
+          <div className="d-block h5 mt-3">{title}</div>
+          <p className="text-sm text-muted">{description}</p>
+          <h6>Stack:</h6>
+          <p className="text-sm text-muted">{stack}</p>
+          <div className="row mt-4">
+            <div className="col-sm-4">
+              <a
+                href={website}
+                target="_blank"
+                rel="noreferrer"
+                className="btn btn-block btn-sm mb-1"
+                id="btn-gradient"
+              >
+                Live website
+              </a>
             </div>
+            {githubButton}
+            {youtubeButton}
+          </div>
         </div>
-    )
-
-}
+      </div>
+    </div>
+  );
+};
 
 export default Card;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -15,64 +15,62 @@ export default class Portfolio extends Component {
         <div className="container">
           <div className="row justify-content-center">
             <div className="col text-center">
-              <h1 id="hero">My portfolio projects_ 👀</h1>
+              <h1 id="hero">👋 Hello, here are some of my projects</h1>
             </div>
           </div>
 
-          <div className="row justify-content-center">
+          <div className="row justify-content">
             <Card
               title="Decentralized Exchange"
-              description="Live on Kovan Ethereum testnet. Deployed ERC20 token with symbol $LMAO. 
-                            Deployed exchange contract. Users can trade $LMAO token againts Testnet ETH. 
-                            Users can place buy/sell orders, or market buy/sell existing orders.
-                            A candle chart is available to show recent trade history."
+              description="A peer-to-peer marketplace where users can trade 
+                cryptocurrencies in a non-custodial manner without the need for 
+                an intermediary to facilitate the transfer and custody of funds.
+                Deployed on Kovan and Polygon testnets"
               stack={
-                "Solidity, React, Redux, Ganache, Infura, Truffle, Bootstrap, Netlify"
+                "Solidity, Ethers.js, React, Redux, Hardhat, Infura, Bootstrap"
               }
-              website="https://lmao.shill.lol"
-              github={"https://github.com/plabradorjr/lmao-token-and-exchange"}
-              image={ethereum}
+              website="https://dex.pancho.lol"
+              github={
+                "https://github.com/plabradorjr/blockchain-developer-bootcampv2"
+              }
+              // image={ethereum}
             />
-          </div>
-          <div className="row justify-content-center">
+
             <Card
               title="Trending Cryptocurrency Currator"
               description="Users can view trending coins from Coingecko API,
-                            search thousands of other cryptocurrencies,
+                            search thousands of cryptocurrencies,
                             post comments, login via twitter, and interact with
                             tradingview candle charts."
               website={"https://app.shill.lol"}
               github={"https://github.com/plabradorjr/shill_v0.2"}
               stack="React, Redux, Ruby on Rails, HighCharts, Bootstrap,
                             API, OAuth gem, Heroku, Netlify"
-              image={portfolio2}
+              // image={portfolio2}
               youtube="https://youtu.be/B1-YHROsui8"
             />
-          </div>
-          <div className="row justify-content-center">
+
             <Card
               title="Content and Photo Management System for Nurses"
               description="Users can create notes and inventories. Allows 
                             image upload hosted by Amazon Web Services.
-                            Secured third party login via Twitter."
+                            Provides secured third party login via Twitter."
               stack={"Ruby on Rails, Amazon Web Services, SQL, Bootstrap"}
               website="https://test1.dogeis.money/"
               github={"https://github.com/plabradorjr/surgi_app_rails"}
-              image={surgiwiki}
+              // image={surgiwiki}
               youtube="https://youtu.be/MkMiXls_E7g"
             />
-          </div>
-          <div className="row justify-content-center">
+
             <Card
               title="E-commerce Store"
               description="Fully functional and automated e-commerce store.
                             Users around the world can purchase t-shirts and
-                            all the fullfillments are automated. Approximate 
-                            delivery in 10-20 business days. Credit cards and 
+                            all the fullfillments are automated. Credit cards and 
                             major cryptocurrencies are accepted as form of payments."
               stack={"Wordpress, Stripe, Coinbase, WooCommerce"}
               website="https://shill.lol"
-              image={portfolio4}
+              // image={portfolio4}
             />
           </div>
         </div>
