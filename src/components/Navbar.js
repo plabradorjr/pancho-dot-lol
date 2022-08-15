8b0eec2f55df655d5330047f5ebe27b0c6e2cd6e
Pancho Labrador <plabradorjr@gmail.com>
2022-08-14 20:48:43 -0500

    simpler nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,48 +5,36 @@ import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-horizontal navbar-expand-lg navbar-dark bg-dark-dark">
-      <div className="container justify-content">
-        <div className="col-md-6">
-          <Link to="/">
-            <p className="navbar-brand mt-3">Pancho Labrador Jr.</p>
-          </Link>
-        </div>
-        <div className="col-md-6 text-right">
-          <Link to="/resume">
-            <span className="navbar-brand" href="#" style={{ fontSize: 16 }}>
-              Resume
-            </span>
-          </Link>
-
-          <a
-            className="navbar-brand"
-            style={{ fontSize: 16 }}
-            href="https://github.com/plabradorjr"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <span className="nav-link-inner--text">Github</span>
-          </a>
-          <a
-            className="navbar-brand"
-            style={{ fontSize: 16 }}
-            href="https://www.linkedin.com/in/plabrador/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <span className="nav-link-inner--text">LinkedIn</span>
-          </a>
-          <a
-            className="navbar-brand text-right"
-            style={{ fontSize: 16 }}
-            href="https://twitter.com/0xSer"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <span className="nav-link-inner--text">Twitter</span>
-          </a>
-        </div>
+    <nav
+      className="navbar navbar-light _dark-nav justify-content-between pt-4 px-5 nav-text"
+      id="nav-box"
+    >
+      <div>
+        <Link to="/">
+          {" "}
+          <h4 id="nav-text">Pancho Labrador Jr.</h4>{" "}
+        </Link>
+        <Link to="/resume">
+          {" "}
+          <span id="_nav-link">Resume</span>{" "}
+        </Link>
+        <a
+          href="https://github.com/plabradorjr"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span id="_nav-link">Github</span>{" "}
+        </a>
+        <a
+          href="https://www.linkedin.com/in/plabrador/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span id="_nav-link">LinkedIn</span>{" "}
+        </a>
+        <a href="https://twitter.com/0xSer" target="_blank" rel="noreferrer">
+          <span id="_nav-link">Twitter</span>{" "}
+        </a>
       </div>
     </nav>
   );
