c4c9b73431f5e8fb200b6e8cb1e58c1dea3c147f
Pancho Labrador <plabradorjr@gmail.com>
2022-09-30 02:20:10 -0500

    removed twitter nav link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,28 +12,25 @@ const Navbar = () => {
       <div>
         <Link to="/">
           {" "}
-          <h4 id="nav-text">Pancho Labrador Jr._</h4>{" "}
+          <h4 id="nav-text">Pancho Labrador Jr.</h4>{" "}
         </Link>
         <Link to="/resume">
           {" "}
-          <span id="_nav-link">Resume_</span>{" "}
+          <span id="_nav-link">Resume</span>{" "}
         </Link>
         <a
           href="https://github.com/plabradorjr"
           target="_blank"
           rel="noreferrer"
         >
-          <span id="_nav-link">Github_</span>{" "}
+          <span id="_nav-link">Github</span>{" "}
         </a>
         <a
           href="https://www.linkedin.com/in/plabrador/"
           target="_blank"
           rel="noreferrer"
         >
-          <span id="_nav-link">LinkedIn_</span>{" "}
-        </a>
-        <a href="https://twitter.com/0xSer" target="_blank" rel="noreferrer">
-          <span id="_nav-link">Twitter_</span>{" "}
+          <span id="_nav-link">LinkedIn</span>{" "}
         </a>
       </div>
     </nav>
